cbcdfbaa2e961fa3da109d487bf475051b7405c6
Radu Ciocan <ciocan@gmail.com>
2022-12-10 21:23:03 +0200

    day 10 (part 2)

--- 2022/Javascript/10.ts
+++ 2022/Javascript/10.ts
@@ -1,12 +1,17 @@
-const instructions = (await Deno.readTextFile("../_input/10_sample.txt"))
+const instructions = (await Deno.readTextFile("../_input/10.txt"))
   .split("\n")
   .map((line) => line.split(" "));
 
+const NUM_ROWS = 6
+const NUM_COLS = 40
+const NUM_PIXELS = NUM_ROWS * NUM_COLS
 let registerX = 1
 let cycle = 1
 let buffer = [0] as number[]
 let strength = 0
 let cycles = [20, 60, 100, 140, 180, 220]
+let display = new Array(NUM_PIXELS).fill(".")
+let pointer = 0
 
 while (buffer.length) {
   const val = buffer.shift()!
@@ -31,7 +36,23 @@ while (buffer.length) {
     strength += (cycle * registerX)
   }
 
+  if (
+    (cycle - 1) % NUM_COLS === registerX - 1 || 
+    (cycle - 1) % NUM_COLS === registerX - 0 || 
+    (cycle - 1) % NUM_COLS === registerX + 1
+  ) {
+    display[cycle - 1] = "#"
+  }
+
   cycle++
 }
 
 console.log("Part 1:", strength)
+
+const showDisplay = () =>
+  Array.from({ length: NUM_ROWS }, (v, i) =>
+   display.slice(i * NUM_COLS, i * NUM_COLS + NUM_COLS).join("")
+  );
+
+console.log("Part 2:")
+showDisplay().map((line) => console.log(line))
