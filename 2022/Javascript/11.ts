bc15705452bd07841e86671ca415240e2d5da813
Radu Ciocan <ciocan@gmail.com>
2022-12-12 09:43:23 +0200

    day 11 (part 2)

--- 2022/Javascript/11.ts
+++ 2022/Javascript/11.ts
@@ -1,5 +1,6 @@
-const monkeys = (await Deno.readTextFile("../_input/11.txt"))
-  .split("\n\n")
+const monkeysText = (await Deno.readTextFile("../_input/11.txt"))
+
+const parseMonkeysText = (text) => text.split("\n\n")
   .map((m) => m.split("\n"))
   .map(monkey => {
     return {
@@ -13,30 +14,40 @@ const monkeys = (await Deno.readTextFile("../_input/11.txt"))
     }
   })
 
-const totalInspects = new Array(monkeys.length).fill(0)
+const getMonkeyBusinessLevel = (monkeys, numRounds, round = 1) => {
+  const totalInspects = new Array(monkeys.length).fill(0)
+  
+  for (let i = 0; i < numRounds; i++) {
+    monkeys.forEach(monkey => {
+      while (monkey.items.length > 0) {
+        const item = monkey.items.shift()
+        if (item) {
+          totalInspects[monkeys.indexOf(monkey)]++
 
-const numRounds = 20
+          let worryLevel = eval(monkey.operation.replaceAll("old" , item))
 
-for (let i = 0; i < numRounds; i++) {
-  monkeys.forEach(monkey => {
-    while (monkey.items.length > 0) {
-      const item = monkey.items.shift()
-      if (item) {
-        totalInspects[monkeys.indexOf(monkey)]++
-        const worryLevel = Math.floor(eval(monkey.operation.replaceAll("old" , item)) / 3)
-        if (worryLevel % monkey.test.divisible === 0) {
-          monkeys[monkey.test.ifTrue].items.push(worryLevel)
-        } else {
-          monkeys[monkey.test.ifFalse].items.push(worryLevel)
+          worryLevel = round === 1 ? Math.floor(worryLevel / 3) : worryLevel %= divisor
+
+          if (worryLevel % monkey.test.divisible === 0) {
+            monkeys[monkey.test.ifTrue].items.push(worryLevel)
+          } else {
+            monkeys[monkey.test.ifFalse].items.push(worryLevel)
+          }
         }
       }
-    }
-  });
+    });
+  }
+  
+  return totalInspects
+    .sort((a, b) => b - a)
+    .slice(0, 2)
+    .reduce((a, b) => a * b)
 }
 
-const monkeyBusinessLevel = totalInspects
-  .sort((a, b) => b - a)
-  .slice(0, 2)
-  .reduce((a, b) => a * b)
-
-console.log("Part 1:", monkeyBusinessLevel)
+let monkeys = parseMonkeysText(monkeysText)
+const part1 = getMonkeyBusinessLevel(monkeys, 20, 1)
+console.log("Part 1:", part1)
+monkeys = parseMonkeysText(monkeysText)
+const divisor = monkeys.reduce((acc, monkey) => acc * monkey.test.divisible, 1);
+const part2 = getMonkeyBusinessLevel(monkeys, 10000, 2)
+console.log("Part 2:", part2)
