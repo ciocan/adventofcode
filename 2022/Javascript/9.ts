b6c78af2a372ea004609ab447f9d48fa91503c43
Radu Ciocan <ciocan@gmail.com>
2022-12-12 21:28:23 +0200

    day 9 (part 1)

--- 2022/Javascript/9.ts
+++ 2022/Javascript/9.ts
@@ -1,19 +1,55 @@
-const _data = (await Deno.readTextFile("../_input/9.txt")).split("\n");
+const moves = (await Deno.readTextFile("../_input/9_test.txt"))
+  .split("\n")
+  .map((line) => line.split(" "))
+  .map(([direction, distance]) => [direction, Number(distance)]);
 
-const data = `R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2`.split("\n");
+const DIRECTIONS = {
+  U: [-1, 0],
+  D: [+1, 0],
+  R: [0, +1],
+  L: [0, -1],
+}
 
-data.forEach((line) => {
-  const [direction, distance] = line.split(" ");
-  console.log(direction, distance);
+let head = [0, 0]
+let tail = head
+const posToStr = ([r, c]: number[]) => `${r}:${c}`
+const visited = new Set([posToStr(head)])
+
+const moveHead = (direction) => {
+  console.log(head)
+  head = [
+    head[0] + DIRECTIONS[direction][0],
+    head[1] + DIRECTIONS[direction][1]
+  ];
+}
+
+const isBehind = (pos = tail) => pos.some((n, i) => Math.abs(n - head[i]) > 1);
+const isDiagonal = () => tail.every((n, i) => n !== head[i])
+
+const moveTail = () => {
+  if (!isBehind()) return
+  if (isDiagonal()) {
+    let move = [-1, -1]
+    if (!isBehind([tail[0] + 1, tail[1] + 1])) move = [1, 1]
+    else if (!isBehind([tail[0] + 1, tail[1] - 1])) move = [1, -1]
+    else if (!isBehind([tail[0] - 1, tail[1] + 1])) move = [-1, 1]
+    tail[0] += move[0]
+    tail[1] += move[1]
+  } else {
+    if (!isBehind([tail[0] + 1, tail[1]])) tail[0] += 1
+    else if (!isBehind([tail[0] - 1, tail[1]])) tail[0] -= 1
+    else if (!isBehind([tail[0], tail[1] + 1])) tail[1] += 1
+    else tail[1] -= 1
+  }
+}
+
+moves.forEach(([direction, distance]) => {
+  for (let i = 1; i <= distance; i++) {
+    moveHead(direction)
+    moveTail()
+    visited.add(posToStr(tail))
+  }
 });
 
-// console.log("Part 1: ", p1)
-// console.log("Part 2: ", p2)
-
+console.log(visited)
+console.log("Part 1:", visited.size)
